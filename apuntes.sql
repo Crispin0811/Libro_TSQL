9adc619223871abd010fd293a61869a26534eb01
Kevin Crispin Gonzales <kcrispin@baufest.com>
2021-09-24 17:01:16 -0500

    pagina 74

---
+++ apuntes.sql
@@ -0,0 +1,27 @@
+--***********************************************
+--              TIPOS DE DATOS
+--***********************************************
+
+-- DATOS DE CARACTERES
+    -- CHAR = es de longitud fija
+    -- VARCHAR = es de lo longitud variable,  administra los espacios en blaco
+
+-- Comulna de tipo TEXT puede almacenar hasta 8kb
+
+--TIPOS DE DATOS PARA FECHA Y HORA
+    -- DATETIME = acepta fecha y hora
+    -- DATETIME2 = acepta fecha, hora y milisegundo
+    -- TIME
+    -- DATE
+
+-- DECIMALES
+    -- decimal (5,2) = permitirá registro de 5 caracteres con dos decimales.
+
+-- ENTEROS
+    -- INT = almacenará numeros enteros de gran cantidad
+    -- TINYINT =  almacenará valor de 0 a 255
+
+
+
+
+
